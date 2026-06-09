bf30f1e83b98fe98bdde9ee0bd8b2af126275e99
VinuthPoojary <vinuthpoojary0@gmail.com>
2026-06-10 00:02:52 +0530

    Set default login token expiration to 5 hours

--- BACKEND/controllers/authController.js
+++ BACKEND/controllers/authController.js
@@ -8,7 +8,7 @@ const sendEmail = require('../services/emailService');
  */
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '7d',
+    expiresIn: process.env.JWT_EXPIRE || '5h',
   });
 };
 
